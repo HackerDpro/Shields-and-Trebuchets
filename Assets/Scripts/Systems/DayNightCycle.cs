6a8cafd91c56213d05ccebd89003174d40b218db
HackerDpro <dportillobeckand@gmail.com>
2025-06-06 13:29:24 +0200

    Eerste test voor commit

---
+++ Assets/Scripts/Systems/DayNightCycle.cs
@@ -0,0 +1,10 @@
+// Assets/Scripts/Systems/DayNightCycle.cs
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    void Update()
+    {
+        // TODO: dag/nacht laten verlopen
+    }
+}
